55dbca34e9f3abf77e9bc6d192950e1b25ee4170
eulises contreras <eulisescontrerasrengifo1@gmail.com>
2017-04-28 00:37:52 +0000

    agregando plantilla dashboard lite

---
+++ application/views/dashboard.php
@@ -0,0 +1,4 @@
+<?php include 'assets/plantillas/header.php' ?>
+
+
+<?php include 'assets/plantillas/footer.php' ?>
---
+++ assets/plantillas/footer.php
@@ -0,0 +1,22 @@
+    </main>
+</div>
+
+<!-- inject:js -->
+<script src="vendor/d3/d3.min.js"></script>
+<script src="vendor/getmdl-select/getmdl-select.min.js"></script>
+<script src="vendor/material-design-lite/material.min.js"></script>
+<script src="vendor/nvd3/build/nv.d3.min.js"></script>
+<script src="vendor/material-design-lite/src/layout/layout2.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/charts/chart1.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/charts/chart2.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/charts/discreteBarChart.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/employer-form/employer-form.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/line-chart/line-chart-nvd3.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/map/maps.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/pie-chart/pie-chart-nvd3.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/table/table.js"></script>
+<script src="vendor/getmdl-dashboard/src/widgets/todo/todo.js"></script>
+<!-- endinject -->
+
+</body>
+</html>
